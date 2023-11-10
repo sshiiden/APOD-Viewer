b132a718b4a0c707b81802d166f2cebf9d5b5300
SANTOS SHIDEN NICHOLAS <19649@EDU.MARCONI>
2023-11-10 10:08:53 +0100

    separazione url

--- AIOD_viewer.py
+++ AIOD_viewer.py
@@ -5,7 +5,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    r = requests.get("https://api.nasa.gov/planetary/apod?date=2017-08-18&api_key=DEMO_KEY").content
+    API_KEY = "DEMO_KEY"
+    date = "2017-08-18"
+    url = f"https://api.nasa.gov/planetary/apod?date={date}&api_key={API_KEY}"
+    r = requests.get(url).content
     return r
 
 if __name__ == "__main__":
