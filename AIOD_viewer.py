6412bdd557cbee91d84eb5670730354fa6c5d777
SANTOS SHIDEN NICHOLAS <19649@EDU.MARCONI>
2023-11-10 09:32:14 +0100

    modulo requests

--- AIOD_viewer.py
+++ AIOD_viewer.py
@@ -1,10 +1,12 @@
 from flask import Flask
+import requests
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello, World!"
+    r = requests.get("https://api.nasa.gov/planetary/apod?date=2017-08-18&api_key=DEMO_KEY").content
+    return r
 
 if __name__ == "__main__":
     app.run(debug=True)
