432529ca498bbd8ec84e18157c6617eb88130f3c
sshiiden <niksashi42@gmail.com>
2023-11-23 18:08:51 +0100

    cambio del filtro

--- APOD_viewer.py
+++ APOD_viewer.py
@@ -11,21 +11,33 @@ def index():
     url = f"https://api.nasa.gov/planetary/apod?api_key={API_KEY}"
 
     today = datetime.datetime.now().date()
-    start_date = today
-    end_date = None
-
+    data = {"filter_type": "single",
+            "start_date": today,
+            "end_date": today,
+            "n_images": 1,
+            "today": today}
+    
     if request.method == "POST":
-        start_date = request.form["start_date"]
-        end_date = request.form["end_date"]
-
-    url += f"&start_date={start_date}"
-    if end_date:
-        url += f"&end_date={end_date}"
+        match request.form["filter_type"]:
+            case "single":
+                url += f"&start_date={request.form["start_date"]}"
+                url += f"&end_date={request.form["start_date"]}"
+            case "range":
+                url += f"&start_date={request.form["start_date"]}"
+                url += f"&end_date={request.form["end_date"]}"
+            case "random":
+                url += f"&count={request.form["n_images"]}"
+        data["filter_type"] = request.form["filter_type"]
+        data["start_date"] = request.form["start_date"]
+        data["end_date"] = request.form["end_date"]
+        data["n_images"] = request.form["n_images"]
+    elif request.method == "GET":
+        url += f"&start_date={today}"
 
     content = requests.get(url).content
     images = json.loads(content)
 
-    return render_template("base.jinja", images=images, today=today, start_date=start_date, end_date=end_date)
+    return render_template("base.jinja", images=images, data=data)
 
 if __name__ == "__main__":
     app.run(debug=True)
