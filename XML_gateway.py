5ab1343ed4caa33d4eebb6c0d572b880b3335d62
sshiiden <niksashi42@gmail.com>
2023-12-07 17:07:38 +0100

    Revert "error handling"
    This reverts commit 1024f01f8fcdcd0123746734bc995eb99ba260b4.

--- XML_gateway.py
+++ XML_gateway.py
@@ -6,28 +6,24 @@ def getData(args):
     url = f"https://api.nasa.gov/planetary/apod?api_key={API_KEY}"
 
     if args:
-        if all(key in args for key in ("date", "start_date", "end_date", "count")):
-            if "date" in args:
-                url += f"&start_date={args.get('date')}"
-                url += f"&end_date={args.get('date')}"
-            if "start_date" in args:
-                url += f"&start_date={args.get('start_date')}"
-            if "end_date" in args:
-                url += f"&end_date={args.get('end_date')}"
-            if "count" in args:
-                url += f"&count={args.get('count')}"
-            return requests.get(url).json()
-        else:
-            return {"code": 400,
-                    "msg": "Bad Request: incorrect field passed."}
+        if args.get("date"):
+            url += f"&start_date={args.get('date')}"
+            url += f"&end_date={args.get('date')}"
+        if args.get("start_date"):
+            url += f"&start_date={args.get('start_date')}"
+        if args.get("end_date"):
+            url += f"&end_date={args.get('end_date')}"
+        if args.get("count"):
+            url += f"&count={args.get('count')}"
+        return requests.get(url).json()
     else:
         return [requests.get(url).json()]
 
 app = Flask(__name__)
 @app.route("/", methods = ["GET"])
 def index():
-    data = getData(request.args)
-    return render_template("xml/base_XML.jinja", data=data)
+    images = getData(request.args)
+    return render_template("xml/xml.jinja", images=images)
 
 if __name__ == "__main__":
     host = "127.0.0.1"
